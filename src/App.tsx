7d9611abb526b62b4a49b4d93593bae26af0f739
devwithnaveed <786muhammadnaveed@gmail.com>
2025-11-06 22:23:23 +0500

    Add main player to first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import {useMemo} from 'react';
 import './App.css';
 import {Panes} from './Panes';
-import {InstructionsIntro} from './panes/InstructionsIntro';
-import {InstructionsFormat} from './panes/InstructionsFormat';
-import {InstructionsSynthesizer} from './panes/InstructionsSynthesizer';
-import {InstructionsLoops} from './panes/InstructionsLoops';
 import {InstructionsPlayer} from './panes/InstructionsPlayer';
 
 function App() {
   const slides = useMemo(
     () => [
-      <InstructionsIntro />,
-      <InstructionsFormat />,
-      <InstructionsSynthesizer />,
-      <InstructionsLoops />,
       <InstructionsPlayer />
     ],
     []
